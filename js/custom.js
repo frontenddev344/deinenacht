557ab30e39f25e683acd58314decfbc8180f6321
frontenddev344@gmail.com <frontenddev344@gmail.com>
2024-07-29 18:38:13 +0530

    update

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+
+class readMore {
+    constructor() {
+        this.content = '.readmore__content';
+        this.buttonToggle = '.readmore__toggle';
+    }
+
+    bootstrap() {
+        this.setNodes();
+        this.init();
+        this.addEventListeners();
+    }
+
+    setNodes() {
+        this.nodes = {
+            contentToggle: document.querySelector(this.content)
+        };
+
+        this.buttonToggle = this.nodes.contentToggle.parentElement.querySelector(this.buttonToggle);
+    }
+
+    init() {
+        const { contentToggle } = this.nodes;
+
+        this.stateContent = contentToggle.innerHTML;
+
+        contentToggle.innerHTML = `${this.stateContent.substring(0, 400)}...`;
+    }
+
+    addEventListeners() {
+        this.buttonToggle.addEventListener('click', this.onClick.bind(this))
+    }
+
+    onClick(event) {
+        const targetEvent = event.currentTarget;
+        const { contentToggle } = this.nodes
+
+        if (targetEvent.getAttribute('aria-checked') === 'true') {
+            targetEvent.setAttribute('aria-checked', 'false')
+            contentToggle.innerHTML = this.stateContent;
+            this.buttonToggle.innerHTML = 'Lese weniger'
+
+        } else {
+            targetEvent.setAttribute('aria-checked', 'true')
+            contentToggle.innerHTML = `${this.stateContent.substring(0, 500)}...`
+            this.buttonToggle.innerHTML = 'Mehr lesen'
+        }
+    }
+}
+
+
+const initReadMore = new readMore();
+initReadMore.bootstrap()
